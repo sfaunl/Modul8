37679dd04d49a96e9e04e37b75118928a2119f7f
Sefa Unal <git@sefaunal.com>
2023-01-05 02:35:33 +0300

    implement emscripten main loop

--- src/app.cpp
+++ src/app.cpp
@@ -1,5 +1,9 @@
 #include "app.h"
 
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
 App* app_init()
 {
     App *app = new App;
@@ -13,15 +17,28 @@ App* app_init()
     return app;
 }
 
+void main_loop(void* arg)
+{
+    App *app = (App*)arg;
+
+    modulation_run(app);
+    gui_run(app);
+}
+
+
 int app_run(void *userArg)
 {
     App *app = (App*)userArg;
+
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop_arg(main_loop, app, 0, 1);
+#else
     // Main loop
     while (app->keepRunning)
     {
-        modulation_run(app);
-        gui_run(app);
+        main_loop(app);
     }
+#endif //__EMSCRIPTEN__
 
     return 0;
 }
